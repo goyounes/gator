9ea561daca3bd6778c05a4269782961b2226b5b4
goyounes <Younes.sahar.ing@gmail.com>
2026-02-05 16:28:51 +0100

    Main() now takes inputs and runs commands

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
-import { Config, readConfig, setUser } from "./config";
+import { CommandsRegistry, handlerLogin, registerCommand, runCommand } from "./commands/commands";
+// import { Config, readConfig, setUser } from "./config";
 
 function main() {
-  console.log("Hello, world!");
-  setUser("Aron")
-  const cfg : Config = readConfig()
-  console.log(cfg)
+    console.log("Hello, world!");
+    const commandsRegistry: CommandsRegistry = {}
+    registerCommand(commandsRegistry, "login", handlerLogin);
+    console.log(commandsRegistry)
+
+    try {
+        if (process.argv.length === 2) {
+            throw new Error("Not enough arguments were provided")
+        }
+        const [ , ,cmdName, ...args] = process.argv 
+        runCommand(commandsRegistry,cmdName,...args)
+    } catch (err) {
+        console.log((err as Error).message)
+        process.exit(1)
+    }
 }
 
 main();
