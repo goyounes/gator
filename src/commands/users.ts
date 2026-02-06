999021a49995ad46c6ce33e21c3f8c956b680f49
goyounes <Younes.sahar.ing@gmail.com>
2026-02-06 15:42:22 +0100

    login function checks user in db / register logins automatically

--- src/commands/users.ts
+++ src/commands/users.ts
@@ -1,11 +1,15 @@
 import { readConfig, setUser } from "src/config"
-import { createUser } from "src/lib/db/queries/users"
+import { createUser, getUserByName } from "src/lib/db/queries/users"
 
 export async function handlerLogin(cmdName:string, ...args:string[]): Promise<void>{
     if (args.length !== 1 ) {
         throw new Error ("login function expects <username>")
     }
     const username = args[0]
+    const user = await getUserByName(username);
+    if (!user) {
+        throw new Error ("login function expects the <username> to exist in the database")
+    }
     setUser(username)
     console.log(`User ${username} has been set`)
     console.log("new config =>", readConfig())
@@ -19,6 +23,7 @@ export async function handlerRegister(cmdName:string, ...args:string[]): Promise
     try {
         await createUser(username);
         console.log(`User ${username} has been created`)
+        setUser(username)
     } catch (err) {
         throw new Error(`Failed to create user ${username}`);
     }
