09835bc376cf65b4428a4bc5644af168761c4a33
goyounes <Younes.sahar.ing@gmail.com>
2026-02-08 18:32:56 +0100

    created feedFollows table

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -23,5 +23,18 @@ export const feeds = pgTable("feeds", {
   userId: uuid("user_id").references(() => users.id,{onDelete: 'cascade'}).notNull(),
 });
 
+ 
+export const feedFollows = pgTable("feed_follows", {
+  id: uuid("id").primaryKey().defaultRandom().notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  userId: uuid("user_id").references(() => users.id,{onDelete: 'cascade'}).notNull(),
+  feedId: uuid("feed_id").references(() => feeds.id,{onDelete: 'cascade'}).notNull(),
+});
+
 export type Feed = typeof feeds.$inferSelect;
 export type User = typeof users.$inferSelect;
+export type FeedFollow = typeof feedFollows.$inferSelect;
