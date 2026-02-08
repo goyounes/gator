c15ddc91c42a487babadcf708272dbfcf95534ac
goyounes <Younes.sahar.ing@gmail.com>
2026-02-08 20:14:40 +0100

    fixed creareFeedFollow return bug

--- src/commands/followCommands.ts
+++ src/commands/followCommands.ts
@@ -13,8 +13,8 @@ export async function handlerFollow(cmdName:string, ...args:string[]): Promise<v
     const currentUser: User = await getUserByName(cfg.currentUserName)
     const feed : Feed = await getFeedByUrl(url)
     try {        
-        const _  = await createFeedFollow(feed.id, currentUser.id)
-        console.log(`Successfully followed feed: ${feed.name} for user: ${currentUser.name}`)
+        const newFeedFollow  = await createFeedFollow(feed.id, currentUser.id)
+        console.log(`Successfully followed feed: ${newFeedFollow.feedName} for user: ${newFeedFollow.userName}`)
     } catch (err) {
         throw new Error(`Failed to follow feed url: ${url}: ${(err instanceof Error) ? err.message : err}`);
     }
--- src/lib/db/queries/feedFollowsQueries.ts
+++ src/lib/db/queries/feedFollowsQueries.ts
@@ -1,10 +1,10 @@
 import { db } from "..";
 import { feedFollows, feeds, users } from "../schema";
-import { eq } from 'drizzle-orm';
+import { eq, and } from 'drizzle-orm';
 
 export async function createFeedFollow(feedId: string, userId : string) {
-    const [ _ ] = await db.insert(feedFollows).values( {feedId: feedId, userId: userId }).returning();
-    const feedFollowsWithNames = await db.select({
+    const [ newFeedFollow ] = await db.insert(feedFollows).values( {feedId: feedId, userId: userId }).returning();
+    const [feedFollowWithNames] = await db.select({
         id: feedFollows.id,
         createdAt: feedFollows.createdAt,
         updatedAt: feedFollows.updatedAt,
@@ -14,9 +14,14 @@ export async function createFeedFollow(feedId: string, userId : string) {
         feedName: feeds.name
     }).from(feedFollows).
     innerJoin(users,eq(feedFollows.userId, users.id)).
-    innerJoin(feeds,eq(feedFollows.feedId, feeds.id))
-    
-    return feedFollowsWithNames
+    innerJoin(feeds,eq(feedFollows.feedId, feeds.id)).
+    where(
+      and(
+        eq(feedFollows.id, newFeedFollow.id),
+        eq(users.id, newFeedFollow.userId),
+      )
+    )
+    return feedFollowWithNames
 }
 
 export async function getFeedFollowsForUser (userId : string) {
