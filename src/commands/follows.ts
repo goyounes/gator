15bbb0f4181d0d002294241bfca730f90599ab30
goyounes <Younes.sahar.ing@gmail.com>
2026-02-08 19:30:57 +0100

    added Following command handler

--- src/commands/follows.ts
+++ src/commands/follows.ts
@@ -1,10 +1,8 @@
 import { Config, readConfig } from "src/config";
 import { getUserByName } from "src/lib/db/queries/users";
-import { Feed, FeedFollow, User } from "src/lib/db/schema";
-import { createFeed, getAllFeeds, getFeedById, getFeedByUrl } from "src/lib/db/queries/feeds";
-import { fetchRSSFeed, printRSSFeed, RSSFeed } from "src/lib/rss";
-import { createFeedFollow } from "src/lib/db/queries/feedFollows";
-
+import { Feed, User } from "src/lib/db/schema";
+import { getFeedByUrl } from "src/lib/db/queries/feeds";
+import { createFeedFollow, getFeedFollowsForUser } from "src/lib/db/queries/feedFollows";
 
 export async function handlerFollow(cmdName:string, ...args:string[]): Promise<void>{
     if (args.length !== 1 ) {
@@ -19,5 +17,20 @@ export async function handlerFollow(cmdName:string, ...args:string[]): Promise<v
         console.log(`Successfully followed feed: ${feed.name} for user: ${currentUser.name}`)
     } catch (err) {
         throw new Error(`Failed to follow feed url: ${url}: ${(err instanceof Error) ? err.message : err}`);
+    }
+}
+
+export async function handlerFollowing(cmdName:string, ...args:string[]): Promise<void>{
+    const cfg: Config = readConfig() 
+    const currentUser: User = await getUserByName(cfg.currentUserName)
+
+    try {        
+        const userFeedFollows  = await getFeedFollowsForUser(currentUser.id)
+        console.log(`found feeds: ${userFeedFollows.length}`)
+        userFeedFollows.forEach( feedFollow  => {
+            console.log(` - ${feedFollow.feedName}`)
+        })
+    } catch (err) {
+        throw new Error(`Failed to fetch feed for user : ${currentUser.name}: ${(err instanceof Error) ? err.message : err}`);
     }
 }
