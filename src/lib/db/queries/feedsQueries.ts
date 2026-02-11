83f8cfb588f14a4f3de9026ff602cc7cfd2e3fc2
goyounes <Younes.sahar.ing@gmail.com>
2026-02-11 10:46:04 +0100

    added queries : markFeedFetched + getNextFeedToFetch

--- src/lib/db/queries/feedsQueries.ts
+++ src/lib/db/queries/feedsQueries.ts
@@ -1,6 +1,6 @@
 import { db } from "..";
 import { Feed, feeds, User, users } from "../schema";
-import { eq } from 'drizzle-orm';
+import { asc, eq, sql } from 'drizzle-orm';
 
 export async function createFeed(name: string, url : string, userId: string) : Promise<Feed> {
     const [result] = await db.insert(feeds).values({ name: name, url: url, userId: userId }).returning();
@@ -25,4 +25,23 @@ export async function getFeedByUrl(url: string): Promise <Feed> {
 export async function getAllFeeds(): Promise < Array<{feeds:Feed; users: User}> > {
     const result = await db.select().from(feeds).innerJoin(users, eq(feeds.userId, users.id))
     return result 
+}
+
+export async function markFeedFetched (feedId: string){
+    const [result] = await db
+        .update(feeds)
+        .set({updatedAt:new Date(), lastFetchedAt:new Date()})
+        .where(eq(feeds.id, feedId))
+        .returning(); 
+
+    return result  
+}
+export async function getNextFeedToFetch (): Promise<Feed> {
+    const mostObsoleteFeed =  await db
+        .select()
+        .from(feeds)
+        .orderBy( sql`${feeds.lastFetchedAt} ASC NULLS FIRST`)
+        .limit(10);   
+    console.log(mostObsoleteFeed)
+    return mostObsoleteFeed[0]
 }
